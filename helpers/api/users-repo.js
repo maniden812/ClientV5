f0539e96b22c533080bde18d621d5cc746ac730f
maniden812 <dendukuri.manasa@gmail.com>
2022-05-04 12:42:11 -0500

    table on the same page as order form, connecting to db, added various functions for connection to db -Manasa

--- helpers/api/users-repo.js
+++ helpers/api/users-repo.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+// const fs = require('fs');
 
 // users in JSON file for simplicity, store in a db for production applications
 let users = require('data/users.json');
@@ -8,6 +8,7 @@ export const usersRepo = {
     getById: id => users.find(x => x.id.toString() === id.toString()),
     find: x => users.find(x),
     create,
+    sale,
     update,
     delete: _delete
 };
@@ -24,7 +25,15 @@ function create(user) {
     users.push(user);
     saveData();
 }
+function sale(id, params) {
+    const user = users.find(x => x.id.toString() === id.toString());
+    // set sale data into db 
+    user.sale = user.sale.append(params);
+    user.dateUpdated = new Date().toISOString();
 
+    users.sale.push(user, params);
+    saveData();
+}
 function update(id, params) {
     const user = users.find(x => x.id.toString() === id.toString());
 
